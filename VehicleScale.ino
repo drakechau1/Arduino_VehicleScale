721c285b7e5c161a4bcf66b6b71ac59962b9a3fd
duccm <ducchau.dev@gmail.com>
2023-12-06 01:54:33 +0700

    add: Init Logging

--- VehicleScale.ino
+++ VehicleScale.ino
@@ -3,6 +3,13 @@
 #include "ArduinoLog.h"
 
 void setup() {
+    // Set up serial port and wait until connected
+  Serial.begin(9600);
+  while (!Serial && !Serial.available()) {}
+
+  // Initialize with log level and log output.
+  Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+
   const Custom_HX711 loadcell;
   loadcell.init();
 }
